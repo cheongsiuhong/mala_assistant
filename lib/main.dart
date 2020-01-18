a02bccc9713c6062bc7d1e77c0f0041eae02a958
Cheong Siu Hong <cheongsiuhong@gmail.com>
2020-01-18 17:15:33 +0800

    file structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp());
+void main() => runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => Splash(),
+      '/home': (context) => Home(),
+      '/stores': (context) => Stores(),
+      '/friends': (context) => Friends(),
+      '/meal': (context) => Meal(),
+    }
+));
 
 
