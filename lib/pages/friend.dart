bee9b228498379c2beaeef72c4a19eb0dd7b2e02
Lim Wen Han Eugene <e0309924@u.nus.edu>
2020-01-19 01:48:35 +0800

    Added Friends add

---
+++ lib/pages/friend.dart
@@ -0,0 +1,9 @@
+class Friend {
+  String name;
+  int spiceLevel;
+  double budget;
+  int appetite;
+
+  Friend(
+      {this.name, this.spiceLevel, this.appetite, this.budget});
+}
--- lib/pages/friends.dart
+++ lib/pages/friends.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mala_assistant/pages/friend.dart';
 
 class Friends extends StatefulWidget {
   @override
@@ -6,8 +7,100 @@ class Friends extends StatefulWidget {
 }
 
 class _FriendsState extends State<Friends> {
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  List<Friend> malaFriends = [
+    Friend(name: 'Eugene', spiceLevel: 1, appetite: 2, budget: 10)
+  ];
+
+  void addFriend(Friend friend) {
+      setState(() => malaFriends.add(friend));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text('Mala buddies :D'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: ListView.builder(
+          itemCount: malaFriends.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                  onTap: () {},
+                  title: Text(malaFriends[index].name)
+              ),
+            );
+          }
+      ),
+      floatingActionButton: new FloatingActionButton(
+          onPressed: addFriendScreen,
+          tooltip: 'Add Friend',
+          child: new Icon(Icons.add)
+      ),
+    );
+  }
+
+  void addFriendScreen() {
+    Friend newFriend = new Friend();
+    Navigator.of(context).push(
+        new MaterialPageRoute(
+            builder: (context) {
+              return new Scaffold(
+                  appBar: new AppBar(
+                      title: new Text('Add a new friend')
+                  ),
+                  body: new Form(
+                      key: _formKey,
+                      autovalidate: true,
+                      child: new ListView(
+                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                          children: <Widget>[
+                            new TextFormField(
+                              decoration: const InputDecoration(
+                                labelText: 'Name',
+                              ),
+                              onSaved: (val) => newFriend.name = val,
+                            ),
+                            new TextFormField(
+                              decoration: const InputDecoration(
+                                labelText: 'Spice Level',
+                              ),
+                              onSaved: (val) =>
+                              newFriend.spiceLevel = int.parse(val),
+                            ),
+                            new TextFormField(
+                              decoration: const InputDecoration(
+                                labelText: 'Budget',
+                              ),
+                              onSaved: (val) =>
+                              newFriend.budget = double.parse(val),
+                            ),
+                            new TextFormField(
+                              decoration: const InputDecoration(
+                                labelText: 'Appetite',
+                              ),
+                              onSaved: (val) =>
+                              newFriend.appetite = int.parse(val),
+                            ),
+                            new Container(
+                                padding: const EdgeInsets.only(
+                                    left: 40.0, top: 20.0),
+                                child: new RaisedButton(
+                                  child: const Text('Submit'),
+                                  onPressed: () {
+                                    final FormState form = _formKey.currentState;
+                                    form.save();
+                                    addFriend(newFriend);
+                                    Navigator.pop(context);
+                                  },
+                                )
+                            )
+                          ])
+                  ));
+            }));
   }
 }
