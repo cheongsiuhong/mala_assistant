e50044f8aad3b241c5bdd9cb69419ab40a234aca
Cheong Siu Hong <cheongsiuhong@gmail.com>
2020-01-18 17:48:39 +0800

    splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:mala_assistant/pages/splash.dart';
 
 void main() => runApp(MaterialApp(
     initialRoute: '/',
     routes: {
       '/': (context) => Splash(),
-      '/home': (context) => Home(),
-      '/stores': (context) => Stores(),
-      '/friends': (context) => Friends(),
-      '/meal': (context) => Meal(),
+//      '/home': (context) => Home(),
+//      '/stores': (context) => Stores(),
+//      '/friends': (context) => Friends(),
+//      '/meal': (context) => Meal(),
     }
 ));
 
--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,1 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Splash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.red[900],
+        body: Center(
+            child: Column(
+              children: <Widget>[
+                Text(
+                  "Mala Assistant",
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 2.0,
+                    fontSize:24.0,
+                  )
+                ),
+                SpinKitThreeBounce(
+                  color: Colors.white,
+                  size: 50.0,
+                ),
+              ],
+            )
+        )
+    );
+  }
+}
