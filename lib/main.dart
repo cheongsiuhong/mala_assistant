a5439525025618308a63edc6d3efb8d6cc72e1f5
Cheong Siu Hong <cheongsiuhong@gmail.com>
2020-01-18 18:20:11 +0800

    redirect to home

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:mala_assistant/pages/splash.dart';
+import 'package:mala_assistant/pages/home.dart';
+
 
 void main() => runApp(MaterialApp(
     initialRoute: '/',
     routes: {
       '/': (context) => Splash(),
-//      '/home': (context) => Home(),
+      '/home': (context) => Home(),
 //      '/stores': (context) => Stores(),
 //      '/friends': (context) => Friends(),
 //      '/meal': (context) => Meal(),
--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,21 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
+
+
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+
+  @override
+  void initState() {
+    super.initState();
+    redirect();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.red[900],
         body: Center(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
                 Text(
                   "Mala Assistant",
                   style: TextStyle(
                     color: Colors.white,
                     letterSpacing: 2.0,
-                    fontSize:24.0,
+                    fontSize:36.0,
+                    fontWeight: FontWeight.bold,
                   )
+                ),
+                SizedBox(
+                  height: 8.0,
                 ),
                 SpinKitThreeBounce(
                   color: Colors.white,
@@ -25,5 +44,10 @@ class Splash extends StatelessWidget {
             )
         )
     );
+  }
+
+  void redirect() async {
+    await Future.delayed(const Duration(seconds:2));
+    Navigator.pushReplacementNamed(context, '/home');
   }
 }
